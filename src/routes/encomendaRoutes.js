a505828e28d920cb2af4d7036c845b998300dd0c
rmo02 <ramoon.maia23@gmail.com>
2023-04-25 11:37:12 -0300

    atualizando cors

--- src/routes/encomendaRoutes.js
+++ src/routes/encomendaRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 //exemplo da rota "/encomenda?codigos=AB123456789BR,CD987654321BR"
 
 router.get('/', async (req, res) => {
-  router.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   const { codigos } = req.query;
   if (!codigos) {
     return res.status(400).json({ message: 'Códigos de encomenda não informados' });
